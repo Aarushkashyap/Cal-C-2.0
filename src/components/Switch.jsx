340b9ae18eccbdd1c0bda64b7af449aaae08b793
Aarush Kashyap <aarush.kashyap21@gmail.com>
2024-05-16 04:02:16 +0530

    Created switch between design among calculator and convertor

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Switch = ({options, selectedIndex, Onclick}) => {
+const Switch = ({options, selectedIndex, onClick}) => {
   return (
     <div className='h-10 overflow-hidden rounded'>
         <div className="relative flex h-full">
@@ -13,15 +13,23 @@ const Switch = ({options, selectedIndex, Onclick}) => {
                              dark:bg-secondary ${index === selectedIndex 
                                 ? "text-white" 
                                 : "text-gray-800 dark:text-gray-400"
-                             }`}
+                             }`} 
+                             onClick={() => onClick(index)}
                         >
                             <span className='z-10 text-xs capitalize transition-colors duration-300'>
                                 {option}
                             </span>
                         </div>
-                    )
-                })
-            }
+                    );
+                })}
+                    <div className="absolute rounded h-full bg-primary transition-all duration-300"
+                    style={{
+                        width:`${100 /options.length}% `,
+                        left: `${(100/options.length) * selectedIndex}%`,
+                    }}
+                >
+
+                    </div>
         </div>
     </div>
   )
